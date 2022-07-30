bed2f4921e58b9a012f76f9fd2ac49113cf65006
Shane Castro <shaneh026@gmail.com>
2022-07-30 18:00:09 -0400

    Created handlers.js file

---
+++ server/handlers.js
@@ -0,0 +1,15 @@
+"use strict";
+const { MongoClient } = require("mongodb");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// use this package to generate unique ids: https://www.npmjs.com/package/uuid
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = {};
