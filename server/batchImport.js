da4f64d89d4993a0ef0a518613b4196983a9f9b4
Shane Castro <shaneh026@gmail.com>
2022-07-30 15:36:33 -0400

    imported data to MongoDB

---
+++ server/batchImport.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+const items = require("./data/items.json");
+const companies = require("./data/items.json");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+console.log("asdasd", MONGO_URI);
+const batchImport = async () => {
+  const client = new MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("GroupECommerce");
+
+  await db.collection("items").insertMany(items);
+
+  await db.collection("companies").insertMany(companies);
+  console.log("Success!");
+  client.close();
+};
+
+// batchImport();
