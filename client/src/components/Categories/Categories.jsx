45f8f463a608e4e85b35a563403f889f6833756c
Aidan Charlton <aidan.charlton613@gmail.com>
2022-08-03 11:30:23 -0400

    Categories PR

---
+++ client/src/components/Categories/Categories.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
+const Categories = () => {
+
+    const [loading, setLoading] = useState(true)
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        setLoading(true)
+        const getCategories = async () => {
+            const requestCategories = await fetch("/category");
+            const requestJson = await requestCategories.json();
+            setCategories(requestJson.data)
+            setLoading(false)
+        }
+        getCategories()
+    }, [])
+    return loading ? <></> : (
+        <>
+        <StyledHeader>Categories</StyledHeader>
+            <CategoryWrapper>
+            {
+                categories.map((category, id) => {
+                    return (
+                    <StyledCard to={`category/${category}`} key={id}>
+                        <p>{category}</p>
+                    </StyledCard>
+                    )
+                })
+            }
+            </CategoryWrapper>
+        </>
+    )
+}
+
+const StyledHeader = styled.h1`
+    color: black;
+    padding: 30px;
+`
+
+const CategoryWrapper = styled.div`
+    justify-content: center;
+    display: flex;
+    width: 100vw;
+    height: 100%;
+    flex-wrap: wrap;
+`
+
+const StyledCard = styled(Link)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 300px;
+    margin: 30px;
+    box-shadow:0px 0px 10px 1px lightgray;
+
+    &:hover {
+    cursor: pointer;
+    background: steelblue;
+    color: white;
+    border-color: lightgrey;
+    transition: all ease 400ms;
+  }
+`
+
+export default Categories
---
+++ client/src/components/Categories/CategoryPage.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react"
+import { useParams, Link } from "react-router-dom"
+import styled from "styled-components"
+
+
+
+const CategoryPage = () => {
+    const {id} = useParams()
+    const reqId = id.toLowerCase()
+    const [loading, setLoading] = useState(true)
+    const [categoryItems, setCategoryItems] = useState()
+
+    useEffect(() => {
+        setLoading(true)
+        const getCategoryItems = async () => {
+            const requestItems = await fetch(`/category/${reqId}`);
+            const requestJson = await requestItems.json();
+            setCategoryItems(requestJson.data);
+            console.log(categoryItems)
+            setLoading(false)
+        }
+        getCategoryItems()
+    }, []);
+
+    return loading ? <></> : (
+        <>
+        <StyledHeader>{id}</StyledHeader>
+        <CategoryWrapper>
+        {
+                categoryItems.map((item, id) => {
+                    return (
+                    <StyledCard to={`/product/${item._id}`} key={id}>
+                        <img src={item.imageSrc}/>
+                        <p>{item.name}</p>
+                        <p>{item.price}</p>
+                        <p>in stock:{item.numInStock}</p>
+                    </StyledCard>)
+                })
+            }
+        </CategoryWrapper>
+        </>
+    )
+}
+
+const StyledHeader = styled.h1`
+    color: black;
+    padding: 30px;
+`
+const CategoryWrapper = styled.div`
+    justify-content: center;
+    display: flex;
+    width: 100vw;
+    height: 100%;
+    flex-wrap: wrap;
+`
+const StyledCard = styled(Link)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 300px;
+    margin: 30px;
+    box-shadow:0px 0px 10px 1px lightgray;
+        &:hover {
+    cursor: pointer;
+    background: steelblue;
+    color: white;
+    border-color: lightgrey;
+    transition: all ease 400ms;
+}
+`
+
+export default CategoryPage
